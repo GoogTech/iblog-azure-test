3a9739a8160e2c4381fc0b6316c0255b11bd7535
YUbuntu7916 <46174312+YUbuntu7916@users.noreply.github.com>
2020-04-08 20:15:01 +0800

    Update TestController.java

--- src/main/java/run/iblog/test/controller/TestController.java
+++ src/main/java/run/iblog/test/controller/TestController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/test")
 public class TestController {
+    private String docs = "https://docs.iblog.run/";
 
     @RequestMapping("/iblog")
     public String show(ModelMap modelMap) {
-        modelMap.addAttribute("intro", "An ultra lightweight blogging system based on springboot with the detailed docs : https://docs.iblog.run/");
+        modelMap.addAttribute("intro", "An ultra lightweight blogging system based on springboot with the detailed docs : " + docs);
         return "index";
     }
 }
